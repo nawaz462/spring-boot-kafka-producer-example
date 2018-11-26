10e8037e66f4a57282b47778d5252954c34e87bb
nawaz462 <shahbaznawaz786@gmail.com>
2018-11-26 15:00:37 +0530

    changes in code

--- src/main/java/com/kafkaExample/springbootkafkaproducerexample/resource/MessageController.java
+++ src/main/java/com/kafkaExample/springbootkafkaproducerexample/resource/MessageController.java
@@ -1,27 +1,37 @@
 package com.kafkaExample.springbootkafkaproducerexample.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kafkaExample.springbootkafkaproducerexample.model.User;
 
 @RestController
 public class MessageController {
-
+	
+	@Value("${kafka.destination.topic.name}")
+	private String DESTINATION_TOPIC;
+	
     @Autowired
     private KafkaTemplate<String, User> kafkaTemplate;
 
-    private static final String TOPIC = "Kafka_Json_Example";
+   /* @GetMapping("/publish/{message}")
+    public String publishMessage(@PathVariable("message") final String name) {
 
-    @GetMapping("/publish/{message}")
-    public String post(@PathVariable("message") final String message) {
-
-//    	kafkaTemplate.send(TOPIC, message);
-        kafkaTemplate.send(TOPIC, new User(message, "sample", 100L));
-
+//    	kafkaTemplate.send("Kafka_example", name);
         return "Published successfully";
+    }*/
+    
+    @PostMapping("/publishJson")
+    public String publishJsonMessage(@RequestBody User user) {
+    	
+    	kafkaTemplate.send(DESTINATION_TOPIC, user);
+		return "Successfully pulbished message to "+ DESTINATION_TOPIC;
+    	
     }
 }
